4839151fe2cdf1d81650c10368ee2154ea3278b7
nagarajumadamshetti <nagarajumadamshetti@gmail.com>
2020-04-06 13:32:35 +0530

    added fetch api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+  componentDidMount = async () => {
+    await fetch("https://pomber.github.io/covid19/timeseries.json")
+      .then(response => response.json())
+      .then(data => {
+        data["Argentina"].forEach(({ date, confirmed, recovered, deaths }) =>
+          console.log(`${date} active cases: ${confirmed - recovered - deaths}`)
+        );
+      });
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>hello</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
